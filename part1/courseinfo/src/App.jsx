39edd02cdb410467cb51ef071eaacd2c684ffc48
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-11-24 16:07:56 -0400

    1c

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,13 +4,30 @@ import Content from './components/Content'
 import Total from './components/Total'
 
 const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header />
-      <Content />
-      <Total />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
