fa57366f3db625a18ca1485b29af70ce745db340
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2023-11-24 11:54:38 -0400

    Project start 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+export default App
