ee87eeee144a4f05995e01f0f1267e29c58578ad
Hector Inglese <hinglese@prolesys.com>
2023-12-08 10:42:29 -0400

    1.14* completed

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,52 +2,54 @@ import { useState } from 'react'
 import ShowcasedText from './components/ShowcasedText';
 
 const App = () => {
+  const [topAnecdote, setTopAnecdote] = useState('');
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [selected, setSelected] = useState(0);
   const anecdotes = [
-    {
-      text: 'If it hurts, do it more often',
-      points: 0
-    },
-    {
-      text: 'Adding manpower to a late software project makes it later!',
-      points: 0
-    },
-    {
-      text: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-      points: 0
-    },
-    {
-      text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-      points: 0
-    },
-    {
-      text: 'Premature optimization is the root of all evil.',
-      points: 0
-    },
-    {
-      text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-      points: 0
-    },
-    {
-      text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-      points: 0
-    },
-    {
-      text: 'The only way to go fast, is to go well.',
-      points: 0
-    },
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
   ];
-  function getRandomInt() {
+  const getRandomInt = () => {
     const min = Math.ceil(0);
-    const max = Math.floor(8);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  const [selected, setSelected] = useState(0)
+    const max = Math.floor(7);
+    const anecdoteIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+    return anecdoteIndex;
+  };
+  const getTopAnecdote = () => {
+    let max = votes[0];
+    let index = 0;
+    for (let i = 1; i < votes.length; i++) {
+      if (votes[i] > max) {
+        max = votes[i];
+        index = i;
+      }
+    }
+    setTopAnecdote(anecdotes[index]);
+  };
+  const voteForAnecdote = () => {
+    const copy = [...votes];
+    copy[selected]++;
+    setVotes(copy);
+    getTopAnecdote();
+  };
   return (
     <div>
+      <h2>Anecdote of the day</h2>
+      <ShowcasedText text={anecdotes[selected]} />
       <br />
-      <ShowcasedText text={anecdotes[selected].text} />
-      <br />
+      <button onClick={() => voteForAnecdote()}>
+        vote
+      </button>
       <button onClick={() => setSelected(getRandomInt())}>next anecdote</button>
+      <br />
+      <h2>Anecdotes with most votes</h2>
+      <p>{topAnecdote ? topAnecdote : ''}</p>
     </div>
   )
 };
--- part1/anecdotes/src/components/ShowcasedText.jsx
+++ part1/anecdotes/src/components/ShowcasedText.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
+
 const ShowcasedText = ({ text }) => {
     return (
         <p>{text}</p>
     );
 };
-
 ShowcasedText.propTypes = {
     text: PropTypes.string.isRequired
 };
