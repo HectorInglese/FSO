bbbe4d63be7a548cdaaab0b02a585fba79ca67f8
Hector Inglese <hinglese@prolesys.com>
2023-12-14 15:58:18 -0400

    [ADD] - Base version of route ando router, need to change to routes folder

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,39 @@
-const App = () => {
+import PropTypes from 'prop-types';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const pages = import.meta.glob("./pages/**/*.jsx", { eager: true });
+const routes = Object.keys(pages).map(
+  path => {
+    let objPath = '/' + path.match(/\.\/pages\/(.*)\.jsx$/)?.[1].toLowerCase();
+    let objComponent = pages[path].default
+    if (objPath == '/home') {
+      objPath = '/'
+    }
+    if (objPath == '/country') {
+      objPath = '/country/:id'
+    }
+    return {
+      path: objPath,
+      Element: objComponent,
+      loader: pages[path]?.loader,
+      action: pages[path]?.action,
+      ErrorBoundary: pages[path]?.ErrorBoundary,
+    };
+  }
+);
+const router = createBrowserRouter(
+  routes.map(({ Element, ...path }) => ({
+    ...path,
+    element: <Element />,
+  }))
+);
+const App = ({ countries }) => {
+  if (countries === null) {
+    return (
+      <div>no countries found</div>
+    );
+  }
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   );
-}; export default App;
+};
+App.propTypes = { countries: PropTypes.array.isRequired }; export default App;
