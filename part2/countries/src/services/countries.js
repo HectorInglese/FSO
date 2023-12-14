477113623608920ccff423cd7b4aa7a260afea2d
Hector Inglese <hinglese@prolesys.com>
2023-12-14 16:00:30 -0400

    [ADD] - Frontend api routes

---
+++ part2/countries/src/services/countries.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
+
+const getAllCountries = async () => {
+    try {
+        const response = await axios.get(`${baseUrl}/all`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+const getCountrieByName = async (name) => {
+    try {
+        const response = await axios.get(`${baseUrl}/name/${name}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+export default {
+    getAllCountries,
+    getCountrieByName
+};
