d54472cc2152ed3d062d458fdc4ee9d4541de090
Hector Inglese <hinglese@prolesys.com>
2023-12-14 15:58:36 -0400

    [Add] - import of local copy of countries

--- part2/countries/src/main.jsx
+++ part2/countries/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import countriesCopy from '../data.json'
+const countriesList = countriesCopy
+ReactDOM.createRoot(document.getElementById('root')).render(<App countries={countriesList} />)
