f348c4e0bb5d29ec18e4cff4e8701f95424ce895
Hector Inglese <hinglese@prolesys.com>
2023-12-18 16:07:01 -0400

    2.19*: Data for countries, step2

--- part2/countries/src/pages/Country.jsx
+++ part2/countries/src/pages/Country.jsx
@@ -4,15 +4,28 @@ import { useEffect, useState } from "react";
 const Country = () => {
     const [country, setCountry] = useState({});
     const { name } = useParams();
-
-    const getCountry = async () => {
-        const country = await getCountryByName(name);
-        setCountry(country);
-    };
     useEffect(() => {
+        const getCountry = async () => {
+            const country = await getCountryByName(name);
+            console.log(country);
+            setCountry(country);
+        };
         getCountry();
-    }, []);
+    }, [name]);
     return (
-        <div>Country {country.name && country.name.common}</div>
+        <section>
+            <h3>Country {country.name && country.name.common}</h3>
+            <h2>Capital : {country.capital && country.capital[0]}</h2>
+            <h3>Languages:</h3>
+            <ul>
+                {Object.values(country.languages).map((language) => (
+                    <li key={language}>- {language}</li>
+                ))}
+            </ul>
+            <img
+                src={country.flags.svg}
+                alt={country.flags.alt}
+            />
+        </section>
     );
 }; export default Country;
