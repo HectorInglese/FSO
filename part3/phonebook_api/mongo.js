cc3c2a2514898067c53bbe8719bfefbec4cf6468
Hector Inglese <hinglese@prolesys.com>
2024-01-02 15:22:20 -0400

    3.12: Command-line database

---
+++ part3/phonebook_api/mongo.js
@@ -0,0 +1,15 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config({
+    path: '.env.local'
+});
+const uri = `mongodb+srv://hectoringlese:${process.env.MONGO_DB_PASS}@part3.mwpsihu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+client.connect(uri).then(() => {
+    console.log('MongoDB connected');
+});
