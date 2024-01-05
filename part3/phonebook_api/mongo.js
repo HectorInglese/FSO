2c9c0a3e9d604989ff864878d030a0756d837354
Hector Manuel Inglese Vivas <hectoringlese@gmail.com>
2024-01-04 22:34:10 -0400

    3.12: Command-line database

--- part3/phonebook_api/mongo.js
+++ part3/phonebook_api/mongo.js
@@ -1,19 +1,62 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 require('dotenv').config({
     path: '.env'
 });
+
 const uri = `mongodb+srv://hectoringlese:${process.env.MONGO_DB_PASS}@part3.mwpsihu.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-console.log(uri);
-client.connect(uri).then(() => {
-    console.log('MongoDB connected');
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
 });
 
-client.close();
-return ;
+const Person = mongoose.model('Person', personSchema);
+
+const getAll = () => {
+    Person.find({})
+        .then(result => {
+            console.log('phonebook:');
+            result.forEach(person => {
+                console.log(`${person.name} ${person.number}`);
+            });
+            mongoose.connection.close();
+            console.log('close connection');
+        })
+        .catch(error => {
+            console.error('Error retrieving data:', error);
+            mongoose.connection.close();
+            console.log('close connection');
+        });
+};
+
+mongoose.set('strictQuery', false);
+mongoose.connect(uri)
+    .then(() => {
+        if (process.argv[2] === undefined || process.argv[3] === undefined) {
+            getAll();
+            return;
+        }
+
+        const newName = process.argv[2];
+        const newNumber = process.argv[3];
+
+        const person = new Person({
+            name: newName,
+            number: newNumber
+        });
+
+        person.save()
+            .then(result => {
+                console.log(`added ${newName} number ${newNumber} to phonebook`);
+                mongoose.connection.close();
+                console.log('close connection');
+            })
+            .catch(error => {
+                console.error('Error saving data:', error);
+                mongoose.connection.close();
+                console.log('close connection');
+            });
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
